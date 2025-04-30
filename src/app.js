e0640e2dd6fae05a8227fd6ff7ff38dce1c34b89
FaxriddinDeveloperr <fazliddinomariipov@gmail.com>
2025-04-30 15:08:03 +0500

    logger qoshilgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-import express from 'express';
-import { config } from 'dotenv';
-import { connectDB } from './db/index.js';
-import adminRouter from './routes/admin.routes.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import { config } from "dotenv";
+import { connectDB } from "./db/index.js";
+import adminRouter from "./routes/admin.routes.js";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
 config();
 
 const app = express();
@@ -11,7 +14,21 @@ const PORT = +process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 await connectDB();
+app.use(morgan("dev"));
 
-app.use('/admin', adminRouter);
+const __dirmane = path.resolve();
+const filename = fs.createReadStream(path.join(__dirmane, "access.log"), {
+  flags: "a",
+});
 
-app.listen(PORT, () => console.log('Server running on port', PORT));
+if (process.env.NODE_ENV) {
+    morgan('combined',{
+        stream:filename
+    })
+} else {
+    morgan.use(morgan('dev'))
+}
+
+app.use("/admin", adminRouter);
+
+app.listen(PORT, () => console.log("Server running on port", PORT));
